cd9eae3e4b6fce6a319528e3da1b4a5d4c20fe98
geeftaugustine@gmail.com <geeftaugustine@gmail.com>
2022-12-12 15:24:32 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int main(void)
-{
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
 
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
   
-  return (0);
-  
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);  
 }

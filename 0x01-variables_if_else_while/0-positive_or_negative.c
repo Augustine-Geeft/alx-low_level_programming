6ad2bcbbe6b8d60cb61d11362ffb4d9cff3c30de
geeftaugustine@gmail.com <geeftaugustine@gmail.com>
2022-12-30 01:00:02 -0800

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,9 @@
 #include <time.h>
 /**
  * main - Entry point
- * return - Always 0 (Success)
+ * Return - Always 0.
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;

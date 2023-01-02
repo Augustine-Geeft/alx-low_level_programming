5569e17d37fbbab9e6f5859f5b807545376b6cd8
geeftaugustine@gmail.com <geeftaugustine@gmail.com>
2023-01-02 15:24:12 -0800

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point.
+ * @n: input to be checked.
+ * Return: n is an absolute value.
+ */
+int _abs(int n)
+{
+	int c;
+
+	c = abs(n);
+	_putchar(c);
+}

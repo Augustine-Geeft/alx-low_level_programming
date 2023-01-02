ce0f6ae4422900e9ca0df3b9e38ad3e8d54f25bc
geeftaugustine@gmail.com <geeftaugustine@gmail.com>
2023-01-02 14:06:44 -0800

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: input to be checked.
+ * Return: 1 if lowercase, 0 if uppercase.
+ */
+int _islower(int c)
+{
+	if (islower('c') > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
